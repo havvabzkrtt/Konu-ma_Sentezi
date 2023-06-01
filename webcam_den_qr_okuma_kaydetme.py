55d0c5a3f0e9d5c2a738a920fce3428607f1bed2
Havva <81237002+havvabzkrtt@users.noreply.github.com>
2023-06-01 16:17:29 +0300

    Create webcam_den_qr_okuma_kaydetme.py

---
+++ webcam_den_qr_okuma_kaydetme.py
@@ -0,0 +1,72 @@
+#webcam
+from __future__ import print_function
+
+import pyzbar.pyzbar as pyzbar
+import numpy as np
+import cv2
+import time
+
+# webcam acilir  
+cap = cv2.VideoCapture(0)
+
+cap.set(3,640)
+cap.set(4,480)
+
+time.sleep(2)
+
+def decode(im) : 
+    # QR kodu bulma
+    decodedObjects = pyzbar.decode(im)
+    # Sonucu yazdirma
+    for obj in decodedObjects:
+        print('Type : ', obj.type)
+        print('Data : ', obj.data,'\n')     
+    return decodedObjects
+
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+while(cap.isOpened()):
+    # frame frame yakalama
+    ret, frame = cap.read()
+    im = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+         
+    decodedObjects = decode(im)
+
+    # koseli(qr kodun doğru bir sekilde algilanmasi icin) seklinde yakala
+    for decodedObject in decodedObjects: 
+        points = decodedObject.polygon
+     
+        if len(points) > 4 : 
+          hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
+          hull = list(map(tuple, np.squeeze(hull)))
+        else : 
+          hull = points;
+         
+        n = len(hull)     
+        # Cerceveyi ciz
+        for j in range(0,n):
+          cv2.line(frame, hull[j], hull[ (j+1) % n], (255,0,0), 3)
+
+        x = decodedObject.rect.left
+        y = decodedObject.rect.top
+
+        print(x, y)
+
+        print('Type : ', decodedObject.type)
+        print('Data : ', decodedObject.data,'\n')
+
+        barCode = str(decodedObject.data)
+        cv2.putText(frame, barCode, (x, y), font, 1, (0,255,255), 2, cv2.LINE_AA)
+               
+    # Cikti frame ini goster
+    cv2.imshow('frame',frame)
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'): # kapatmak icin 'q' a bas
+        break
+    elif key & 0xFF == ord('s'): # kaydetmek icin 's' e bas 
+        cv2.imwrite('qr.png', frame)     
+        
+
+cap.release()
+cv2.destroyAllWindows()
